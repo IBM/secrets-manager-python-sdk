7ae51ec34280b2d0eb5ab49c30628fcc26bfddc1
Cadan <cadan.ojalvo@il.ibm.com>
2021-09-19 22:45:07 +0300

    update version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ import os
 import sys
 import pkg_resources
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 PACKAGE_NAME = 'ibm_secrets_manager_sdk'
 PACKAGE_DESC = 'IBM Cloud Secrets Manager Python SDK'
 
