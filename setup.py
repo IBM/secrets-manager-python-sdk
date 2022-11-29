ba20cdd608baca02e891acb0cf3fe4774cb03e6d
yuval-shlomo-dekel <yuval.shlomo.dekel@ibm.com>
2022-11-29 12:55:17 +0200

    fix: releases align release number

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ import os
 import sys
 import pkg_resources
 
-__version__ = '1.0.9'
+__version__ = '1.0.10'
 PACKAGE_NAME = 'ibm_secrets_manager_sdk'
 PACKAGE_DESC = 'IBM Cloud Secrets Manager Python SDK'
 
