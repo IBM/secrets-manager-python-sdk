0eee33f1b04a1fb28845079126dec0150d248393
secrets.automation.dev <secrets.automation.dev@il.ibm.com>
2026-03-16 06:17:22 +0000

    Update version 2.1.14 -> 2.1.15

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ import os
 import sys
 import pkg_resources
 
-__version__ = '2.1.14'
+__version__ = '2.1.15'
 PACKAGE_NAME = 'ibm_secrets_manager_sdk'
 PACKAGE_DESC = 'IBM Cloud Secrets Manager Python SDK'
 
